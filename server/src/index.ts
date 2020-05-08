3669d70064c58616c740860a993be8eb79185db5
greg-n <greg@noonan.be>
2020-05-08 16:27:36 -0400

    better setup / teardown

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,24 @@
-import { setUpServer } from "./server";
+import { setUpServer, tearDownServerItems } from "./server";
+
+const signalsToCatch: NodeJS.Signals[] = ["SIGTERM", "SIGINT"];
 
 setUpServer()
+    .then((serverItems) => {
+        let tornDown = false;
+        for (const signal of signalsToCatch) {
+            process.once(signal, () => {
+                if (tornDown)
+                    return;
+                else
+                    tornDown = true;
+
+                tearDownServerItems(serverItems)
+                    .catch((error) => {
+                        console.error("Error on shutdown", error);
+                    });
+            });
+        }
+    })
     .catch((error) => {
-        console.error("Encountered", error);
+        console.error("Error on starting", error);
     });
